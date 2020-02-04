e97ee7f58340716008499f02d19585afa0742210
Eugen Eisler <eoeisler@gmail.com>
2020-02-04 17:58:23 +0100

    improve html email

--- layouts/_text/partials/emailbridge.js
+++ layouts/_text/partials/emailbridge.js
@@ -26,7 +26,10 @@
     }
 
     function buildEmailBody() {
-        var ret = `<html><body>
+        var ret = `<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+        <html>
+        <head><meta http-equiv="content-type" content="text/html"; charset=ISO-8859-1"></head>
+        <body>
         <h1>Predigtbewertung</h1>
         <h2>${ emailSubject }</h2>
         <span><i>Skala 1 bis 5 (5 = Bester Wert)</i></span>
